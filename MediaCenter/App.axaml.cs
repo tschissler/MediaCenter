4d0bd9cbbe33dae453284ab1a0f74cbb6b54e383
Thomas Schissler <thomas@agilemax.de>
2021-05-02 09:43:51 +0200

    Add project files.

---
+++ MediaCenter/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using MediaCenter.ViewModels;
+using MediaCenter.Views;
+
+namespace MediaCenter
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ MediaCenter/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaCenter.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
