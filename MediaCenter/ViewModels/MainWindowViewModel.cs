aa26dc89dd963fd21d08c8da53d216af9575ff61
Thomas Schissler <thomas@agilemax.de>
2021-05-03 21:55:59 +0200

    Switched to test file

--- MediaCenter/ViewModels/MainWindowViewModel.cs
+++ MediaCenter/ViewModels/MainWindowViewModel.cs
@@ -159,13 +159,13 @@ namespace MediaCenter.ViewModels
             {
                 if (Bass.Init())
                 {
-                    //musicStream = Bass.CreateStream("test.mp3");
-                    musicStream = Bass.CreateStream("https://swr-edge-2034-dus-lg-cdn.cast.addradio.de/swr/swr1/bw/aac/96/stream.aac",
-                        0,
-                        BassFlags.StreamDownloadBlocks | BassFlags.StreamStatus | BassFlags.AutoFree | BassFlags.AacStereo, 
-                        (buffer, length, user) => { },
-                        new IntPtr(r)
-                    );
+                    musicStream = Bass.CreateStream("test.mp3");
+                    //musicStream = Bass.CreateStream("https://swr-edge-2034-dus-lg-cdn.cast.addradio.de/swr/swr1/bw/aac/96/stream.aac",
+                    //    0,
+                    //    BassFlags.StreamDownloadBlocks | BassFlags.StreamStatus | BassFlags.AutoFree | BassFlags.AacStereo, 
+                    //    (buffer, length, user) => { },
+                    //    new IntPtr(r)
+                    //);
 
                     if (musicStream != 0)
                         Bass.ChannelPlay(musicStream); // Play the stream
